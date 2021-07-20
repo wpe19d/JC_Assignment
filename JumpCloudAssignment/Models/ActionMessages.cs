601ec41bae0b59a8f265f307581f7dcbb151c1d7
wpe19d <WesEaston>
2021-07-19 23:26:05 -0500

    Added Action Messages Model

---
+++ JumpCloudAssignment/Models/ActionMessages.cs
@@ -0,0 +1,19 @@
+﻿namespace JumpCloudAssignment.Models
+{
+    public static class ActionMessages
+    {
+        public const string SuccessMessage = "";
+        public const string InvalidAction = "ActionInfo provided is not valid";
+        public const string EmptyAction = "ActionInfo cannot be empty";
+
+        /// <summary>
+        /// Returns a formatted invalid action message with the action input.
+        /// </summary>
+        /// <param name="action">Action string</param>
+        /// <returns>Invalid action message</returns>
+        public static string InvalidActionMessage(string action)
+        {
+            return $"{InvalidAction}. Invalid ActionInfo: {action}";
+        }
+    }
+}
